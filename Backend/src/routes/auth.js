011e2a63201b57ee0fe66aa9af98360e0e1bf197
Jayneel Mahival <jm.mahival@gmail.com>
2025-08-28 00:24:28 +0530

    tested backend -> works

--- Backend/src/routes/auth.js
+++ Backend/src/routes/auth.js
@@ -1,10 +1,26 @@
 import { Router } from "express";
+import { PrismaClient } from "../../generated/prisma/client.js";
 
+const prisma = new PrismaClient();
 const authRouter = Router();
 
-authRouter.get('/signup', async (req, res) => {
-   res.json("Signup route");
+authRouter.post('/signup', async (req, res) => {
+   const { name, email, password } = req.body;
+
+   try {
+      const user = await prisma.user.create({
+         data: {
+            name,
+            email,
+            password
+         }
+      });
+      res.status(201).json(user);
+   } catch (error) {
+      res.status(500).json({ error: "Error creating user" });
+   }
 });
+
 
 authRouter.get('/signin', async (req, res) => {
    res.send("Signin route");
